bdbb480c956fcc0863a6a6cc35db69c465cc5ffd
mshabiola <mshdabiola@gmal.com>
2023-11-02 00:42:28 +0100

    add font

--- modules/designsystem/src/desktopMain/kotlin/com/mshdabiola/designsystem/theme/util/FontResources.desktop.kt
+++ modules/designsystem/src/desktopMain/kotlin/com/mshdabiola/designsystem/theme/util/FontResources.desktop.kt
@@ -1,17 +1,13 @@
 package com.mshdabiola.designsystem.theme.util
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.platform.Font
 
-//
-///**
-// * Make sure you fonts are inside a folder called "font"
-// */
 @Composable
-expect fun fontResources(
+actual fun fontResources(
     font: String,
     weight: FontWeight,
     style: FontStyle
-): Font
+)=  Font("font/$font", weight, style)
