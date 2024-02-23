89b8ef8583d722aaf097070bc236dee95f914f5c
mshdabiola <mshdabiola@gmail.com>
2024-02-23 09:42:29 +0100

    add desktop

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -48,6 +48,13 @@ kotlin {
         }
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
+            implementation(project(":modules:designsystem"))
+            implementation(project(":modules:app"))
+            implementation(libs.kotlinx.coroutines.swing)
+
+            implementation(libs.decompose.core)
+            implementation(libs.decompose.compose.jetbrains)
+            implementation(libs.koin.core)
         }
     }
 }
@@ -88,15 +95,58 @@ android {
 
 compose.desktop {
     application {
-        mainClass = "MainKt"
+        mainClass = "com.mshdabiola.desktop.MainAppKt"
+        /*
+        * Its unreliable. Don't run release tasks for now.
+        * Wait until fixed: https://github.com/JetBrains/compose-jb/issues/2393
+        */
 
+        buildTypes.release.proguard {
+            configurationFiles.from(project.file("compose-desktop.pro"))
+            obfuscate.set(true)
+            version.set("7.3.0")
+        }
+
+        val iconsRoot = project.file("src/main/resources/drawables/launcher")
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "com.mshdabiola.skeletonapp"
-            packageVersion = "1.0.0"
+            packageVersion = "1.0.1"
+            packageName = "Skeleton"
+            description = "Template"
+            copyright = "© 2022 Mshdabiola. All rights reserved."
+            vendor = "Mshdabiola App"
+            version = "1.0.1"
+            licenseFile.set(rootProject.file("LICENSE"))
+
+            modules("java.net.http", "java.sql")
+
+            linux {
+                iconFile.set(iconsRoot.resolve("linux.png"))
+                debMaintainer = "mshdabiola@gmail.com"
+                menuGroup = packageName
+                appCategory = "Productivity"
+            }
+
+            windows {
+                iconFile.set(iconsRoot.resolve("windows.ico"))
+                shortcut = true
+                menuGroup = packageName
+                //https://wixtoolset.org/documentation/manual/v3/howtos/general/generate_guids.html
+                upgradeUuid = "791AC64E-C9A7-4CBF-A1C4-AFE5CFFDDDFA"
+            }
+
+            macOS {
+                iconFile.set(iconsRoot.resolve("macos.icns"))
+                bundleID = "com.mshdabiola.skeleton"
+                appCategory = "public.app-category.productivity"
+                signing {
+                    sign.set(false)
+                }
+            }
         }
     }
 }
+
 
 compose.experimental {
     web.application {}
